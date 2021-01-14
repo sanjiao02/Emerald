841cbd8ddfa3fc3ceee977490416a28cec615b5b
Stupple <Stupple@users.noreply.github.com>
2021-01-14 14:39:12 +0000

    Sound setting added
    biase Sound setting added (dose not save) good for sound testing for objects/ ui/ player

---
+++ Emerald/Assets/Scripts/Sound/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    [SerializeField] string volumeName = "MasterVolume";
+    // Start is called before the first frame update
+
+    public void SetLevel(float Slidervalue)
+    {
+        mixer.SetFloat(volumeName, Mathf.Log10(Slidervalue) * 20);
+    }
+}
