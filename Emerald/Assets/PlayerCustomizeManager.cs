ba9107ec6769f25e4544bd3618742a2bf5a578f6
Lilcooldoode <lilcooldoode@gmail.com>
2021-01-25 23:34:08 +0000

    Character customize UI 99% finished

--- Emerald/Assets/PlayerCustomizeManager.cs
+++ Emerald/Assets/PlayerCustomizeManager.cs
@@ -11,15 +11,28 @@ public class PlayerCustomizeManager : MonoBehaviour
     public GameObject HairGrid;
     public MirSelectButton FaceTab;
     public GameObject FaceGrid;
+    public MirSelectButton HairColourTab;
+    public GameObject HairColourGrid;
 
     private List<GameObject> hairImages = new List<GameObject>();
     private List<GameObject> faceImages = new List<GameObject>();
+    private List<GameObject> haircolourImages = new List<GameObject>();
 
     public List<Sprite> WarriorHairImages = new List<Sprite>();
-    public List<Sprite> WarriorFaceImages = new List<Sprite>();
+    public List<Sprite> WarriorFaceImages = new List<Sprite>();    
+    public List<Sprite> WizardHairImages = new List<Sprite>();
+    public List<Sprite> WizardFaceImages = new List<Sprite>();
+    public List<Sprite> TaoistHairImages = new List<Sprite>();
+    public List<Sprite> TaoistFaceImages = new List<Sprite>();
+    public List<Sprite> AssassinHairImages = new List<Sprite>();
+    public List<Sprite> AssassinFaceImages = new List<Sprite>();
+    public List<Sprite> ArcherHairImages = new List<Sprite>();
+    public List<Sprite> ArcherFaceImages = new List<Sprite>();
+
+    public List<Sprite> HairColourImages = new List<Sprite>();
 
     [HideInInspector]
-    public int SelectedHair, SelectedFace;
+    public byte SelectedHair, SelectedFace, SelectedHairColour;
 
     public void Refresh(MirClass selectedClass, MirGender selectedGender)
     {
@@ -31,31 +44,79 @@ public class PlayerCustomizeManager : MonoBehaviour
             Destroy(faceImages[i]);
         faceImages.Clear();
 
+        for (int i = 0; i < haircolourImages.Count; i++)
+            Destroy(haircolourImages[i]);
+        haircolourImages.Clear();
+
+        List<Sprite> hairList = null;
+        List<Sprite> faceList = null;
+        List<Sprite> haircolourList = null;
+
         switch (selectedClass)
         {
             case MirClass.Warrior:
-                for (int i = (int)selectedGender; i < WarriorHairImages.Count; i += 2)
-                {
-                    GameObject prefab = Instantiate(HairImagePrefab, HairGrid.transform, false);
-                    prefab.GetComponent<Image>().sprite = WarriorHairImages[i];
-                    int x = new int();
-                    x = i / 2;
-                    prefab.GetComponent<HairImageInfo>().Index = x;
-                    prefab.GetComponent<Button>().onClick.AddListener(() => HairImage_onClick(x));
-                    hairImages.Add(prefab);
-                }
-
-                for (int i = (int)selectedGender; i < WarriorFaceImages.Count; i += 2)
-                {
-                    GameObject prefab = Instantiate(HairImagePrefab, FaceGrid.transform, false);
-                    prefab.GetComponent<Image>().sprite = WarriorFaceImages[i];
-                    int x = new int();
-                    x = i / 2;
-                    prefab.GetComponent<HairImageInfo>().Index = x;
-                    prefab.GetComponent<Button>().onClick.AddListener(() => FaceImage_onClick(x));
-                    faceImages.Add(prefab);
-                }
+                hairList = WarriorHairImages;
+                faceList = WarriorFaceImages;                
                 break;
+            case MirClass.Wizard:
+                hairList = WizardHairImages;
+                faceList = WizardFaceImages;
+                break;
+            case MirClass.Taoist:
+                hairList = TaoistHairImages;
+                faceList = TaoistFaceImages;
+                break;
+            case MirClass.Assassin:
+                hairList = AssassinHairImages;
+                faceList = AssassinFaceImages;
+                break;
+            case MirClass.Archer:
+                hairList = ArcherHairImages;
+                faceList = ArcherFaceImages;
+                break;
+        }
+        haircolourList = HairColourImages;
+
+        if (hairList != null)
+        {
+            for (int i = (byte)selectedGender; i < hairList.Count; i += 2)
+            {
+                GameObject prefab = Instantiate(HairImagePrefab, HairGrid.transform, false);
+                prefab.GetComponent<Image>().sprite = hairList[i];
+                int x = new int();
+                x = i / 2;
+                prefab.GetComponent<HairImageInfo>().Index = x;
+                prefab.GetComponent<Button>().onClick.AddListener(() => HairImage_onClick(x));
+                hairImages.Add(prefab);
+            }
+        }
+
+        if (faceList != null)
+        {
+            for (int i = (int)selectedGender; i < faceList.Count; i += 2)
+            {
+                GameObject prefab = Instantiate(HairImagePrefab, FaceGrid.transform, false);
+                prefab.GetComponent<Image>().sprite = faceList[i];
+                int x = new int();
+                x = i / 2;
+                prefab.GetComponent<HairImageInfo>().Index = x;
+                prefab.GetComponent<Button>().onClick.AddListener(() => FaceImage_onClick(x));
+                faceImages.Add(prefab);
+            }
+        }
+
+        if (haircolourList != null)
+        {
+            for (int i = (int)selectedGender; i < haircolourList.Count; i += 2)
+            {
+                GameObject prefab = Instantiate(HairImagePrefab, HairColourGrid.transform, false);
+                prefab.GetComponent<Image>().sprite = haircolourList[i];
+                int x = new int();
+                x = i / 2;
+                prefab.GetComponent<HairImageInfo>().Index = x;
+                prefab.GetComponent<Button>().onClick.AddListener(() => HairColourImage_onClick(x));
+                haircolourImages.Add(prefab);
+            }
         }
 
         HairTab.Select(true);
@@ -63,18 +124,27 @@ public class PlayerCustomizeManager : MonoBehaviour
 
         FaceTab.Select(false);
         FaceGrid.SetActive(false);
+
+        HairColourTab.Select(false);
+        HairColourGrid.SetActive(false);
     }
 
     void HairImage_onClick(int index)
     {
-        SelectedHair = index;
+        SelectedHair = (byte)index;
         Debug.Log("Hair: " + index);
     }
 
     void FaceImage_onClick(int index)
     {
-        SelectedFace = index;
+        SelectedFace = (byte)index;
         Debug.Log("Face: " + index);
+    }
+
+    void HairColourImage_onClick(int index)
+    {
+        SelectedHairColour = (byte)index;
+        Debug.Log("Hair Colour: " + index);
     }
 
     public void HairTab_onClick()
@@ -84,6 +154,9 @@ public class PlayerCustomizeManager : MonoBehaviour
 
         FaceTab.Select(false);
         FaceGrid.SetActive(false);
+
+        HairColourTab.Select(false);
+        HairColourGrid.SetActive(false);
 
         GetComponent<ScrollRect>().verticalScrollbar.value = 1;
     }
@@ -95,6 +168,23 @@ public class PlayerCustomizeManager : MonoBehaviour
 
         HairTab.Select(false);
         HairGrid.SetActive(false);
+
+        HairColourTab.Select(false);
+        HairColourGrid.SetActive(false);
+
+        GetComponent<ScrollRect>().verticalScrollbar.value = 1;
+    }
+
+    public void HairColourTab_onClick()
+    {
+        HairColourTab.Select(true);
+        HairColourGrid.SetActive(true);
+
+        FaceTab.Select(false);
+        FaceGrid.SetActive(false);
+
+        HairTab.Select(false);
+        HairGrid.SetActive(false);        
 
         GetComponent<ScrollRect>().verticalScrollbar.value = 1;
     }
